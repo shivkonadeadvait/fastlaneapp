dde91f6463d6c20b39bf00ae7a913e8ff8e5de75
shivkonadeadvait <shiv.konde@advait.org.in>
2026-06-06 17:44:23 +0530

    chore: bump version to 1.0.3 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.fastlane.testapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
